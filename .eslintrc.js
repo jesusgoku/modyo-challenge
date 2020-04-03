798bc106eb30661a3010e1984b6e407dbb4f47c9
Jesús Urrutia <jesus.urrutia@gmail.com>
2020-04-03 03:27:10 -0300

    chore: eslint setting absolute imports and overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,20 @@
 module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
   // Add custom rules here
-  rules: {}
+  rules: {},
+  overrides: [
+    {
+      files: ['webpack.config.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 0,
+      },
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'webpack.config.js',
+      },
+    },
+  },
 };
